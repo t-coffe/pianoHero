a4d66e3441ccf28e4c9cf668b85cb10ed87ab99e
t-coffe <tcoffe@edenschool.fr>
2024-04-10 15:54:33 +0200

    piano keys appearing in terms of how many octave we want

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 import './style.css'
 
 const body = document.querySelector('body');
+const piano = document.querySelector('div.piano');
 
+
+function KeyWB(nbWB) {
+  for (let i = 0; i < nbWB; i++) {
+    let Wkey = document.createElement("div");
+    Wkey.classList.add("white");
+    let Bkey = document.createElement("div");
+    Bkey.classList.add("black");
+    Wkey.appendChild(Bkey);
+    piano.appendChild(Wkey);
+  }
+  let Wkey = document.createElement("div");
+  Wkey.classList.add("white");
+  piano.appendChild(Wkey);
+}
+
+
+function octave(octaveNB) {
+  for (let i = 0; i < octaveNB; i++) {
+    KeyWB(2);
+    KeyWB(3);
+  }
+  let Wkey = document.createElement("div");
+  Wkey.classList.add("white");
+  piano.appendChild(Wkey);
+}
+
+octave(2);
 
